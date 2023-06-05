01763fcf04f07c406e51a45073834f0b5bdde070
Mehmet Emin Yildiz <mhmtxyildiz@gmail.com>
2023-06-05 11:48:50 +0300

    TodoUseCases+

--- app/src/main/java/com/my/todoclean/feature_todo/domain/use_case/TodoUseCases.kt
+++ app/src/main/java/com/my/todoclean/feature_todo/domain/use_case/TodoUseCases.kt
@@ -3,4 +3,6 @@ package com.my.todoclean.feature_todo.domain.use_case
 data class TodoUseCases(
     val getTodos: GetTodos,
     val deleteTodo: DeleteTodo,
+    val addTodo: AddTodo,
+    val getTodo: GetTodo
 )
